5dcd1af7b1e0abb42c88287dfd93c02aea3af2df
Senthil Natesan <senthil.natesan@atos.net>
2023-07-15 18:25:18 +0530

    today

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -27,3 +27,21 @@ output "example_list_op" {
     value = "Welcome to ${var.example_list[1]} with ${var.example_list[0]} Training and this is our batch ${var.example_list[2]} and the current topic is ${var.example_list[4]}"
   
 }
+
+
+# MAP decleartion in Terraform
+
+variable "devops54_test" {
+    default = {
+        BatchName = "Devops"
+        No = 55
+        Time = "Morning"
+        Duration = "3 month"
+    }
+  
+}
+
+output "devops54_test_class" {
+    value ="Welcome to ${devops54_test[BatchName] } no ${devops54_test[No] } at ${devops54_test[Time] } duration ${devops54_test[Duration] } "
+  
+}
