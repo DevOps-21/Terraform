edc9703df41f046321081de474a208deb974b783
Senthil Natesan <senthil.natesan@atos.net>
2023-07-15 17:12:19 +0530

    today

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -10,3 +10,19 @@ output "sample_var_op"{
     value = "Value of the veriable is ${var.sample_op}"
 }
 
+# list varibale create 
+
+variable "example" {
+    default = [
+        "welcome",
+        "to",
+        "india",
+        1947
+    ]
+  
+}
+
+output "example_op_list" {
+    value = "I am in ${var.eample[2]} proud to ${var.example[0]} to ${var.example[3]}"
+  
+}
