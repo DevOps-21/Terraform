e64d58a80a06109bdfcb61a15e724e8d19c95059
Senthil Natesan <senthil.natesan@atos.net>
2023-07-20 16:03:08 +0530

    18th S3 create security group add in chnage ec2  create in terraform

--- 04-example/ec2.tf
+++ 04-example/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0c1d144c8fdd8d690"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = "HelloWorld"
@@ -9,3 +10,29 @@ resource "aws_instance" "web" {
 
 
 # 14th july class
+
+# create secuirty group
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
