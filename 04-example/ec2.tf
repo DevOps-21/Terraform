0a8b5e2008268bb6100fdfb560c30d8a98d64061
Senthil Natesan <senthil.natesan@atos.net>
2023-07-20 16:05:27 +0530

    18th S3 create security group add in chnage ec2  create in terraform

--- 04-example/ec2.tf
+++ 04-example/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "app_server" {
   ami           = "ami-0c1d144c8fdd8d690"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
